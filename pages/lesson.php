80b263b057a14118cf2fb0e0f735c0a03e15b609
Bakary Keita <bakarykeita77@gmail.com>
2021-10-30 09:37:44 +0000

    Ajout de ngrok wq

--- pages/lesson.php
+++ pages/lesson.php
@@ -42,100 +42,117 @@ if(isset($_SESSION['connected'])){
       <!-------------------------------------------------------------------->
         <span class="fermeture" id="course_fermeture">&times;</span>
       <!-------------------------------------------------------------------->
-                <div id="parametres">
-                    <div id='lesson_parametres_glissiere'>
-                        <table id='table1'>
-                            <tr id='tr11'>
-                                <td id='consonnes_checker'></td>
-                                <td id='tedo_checker'></td>
-                                <td id='voyelles_checker'></td>
-                                <td id='tons_checker'></td>
-                                <td id='nasalisation_checker'></td>
-                            </tr>
-                        </table>
-                        <table id='table2'><tr><td><input type='submit' name='submit_btn' value='ߏ߬ ߛߓߍߦߊ߫. ' id='submit_btn'></td></tr></table>
-                    </div>
-              
-                     <div style='display:none'>
-                        <div id='voyelles_cochees'></div>
-                        <div id='consonnes_cochees'></div>
-                        <div id='tedos_coches'></div>
-                        <div id='tons_coches'></div>
-                        <div id='nasalisations_cochees'></div>
-                    </div>
-                </div>
+        <div id="parametres">
+            <div id='lesson_parametres_glissiere'>
+                <table id='table1'>
+                    <tr id='tr11'>
+                        <td id='consonnes_checker'></td>
+                        <td id='tedo_checker'></td>
+                        <td id='voyelles_checker'></td>
+                        <td id='tons_checker'></td>
+                        <td id='nasalisation_checker'></td>
+                    </tr>
+                </table>
+                <table id='table2'><tr><td><input type='submit' name='submit_btn' value='ߏ߬ ߛߓߍߦߊ߫. ' id='submit_btn'></td></tr></table>
+            </div>
+      
+             <div style='display:none'>
+                <div id='voyelles_cochees'></div>
+                <div id='consonnes_cochees'></div>
+                <div id='tedos_coches'></div>
+                <div id='tons_coches'></div>
+                <div id='nasalisations_cochees'></div>
+            </div>
+        </div>
       <!-------------------------------------------------------------------->
-                <div class="course" id="lesson">
-                    <div class="course_head" id="lesson_entete"></div> 
-                    <div class="course_body" id="lesson_corps"></div>
-    	            <div class='lesson_progress_bar' style="position:absolute; bottom:0; box-shadow:0 0 4px #999; border:1px solid #ddd; border-radius:6px; height:8px; width:calc(100% - 2px)">
-    	                <span class='lesson_progress_question_bar' style="position:absolute; border-radius:6px; right:0; height:8px; width:0; background-color:#ddd; transition:0.6s"></span>
-    	                <span class='lesson_progress_bonne_reponse_bar' style="position:absolute; border-radius:6px; right:0; height:8px; width:0; background-color:yellow; transition:0.6s"></span>
-    	            </div>
+        <div class="course" id="lesson">
+            <div class="course_head" id="lesson_entete"></div> 
+            <div class="course_body" id="lesson_corps"></div>
+            <div class='lesson_progress_bar' style="position:absolute; bottom:0; box-shadow:0 0 4px #999; border:1px solid #ddd; border-radius:6px; height:8px; width:calc(100% - 2px)">
+                <span class='lesson_progress_question_bar' style="position:absolute; border-radius:6px; right:0; height:8px; width:0; background-color:#ddd; transition:0.6s"></span>
+                <span class='lesson_progress_bonne_reponse_bar' style="position:absolute; border-radius:6px; right:0; height:8px; width:0; background-color:yellow; transition:0.6s"></span>
+            </div>
+            <div style='display:none'>
+                <form id='course_form' method='POST' action=''>
+                    <input type='text' name='niveau' id='niveau' value='<?=$niveau?>'/>
+                    <input type='text' name='course_input' id='course_input'/>
+                    <input type='submit' name='submit_course' id='submit_course'/>
+                </form>
+            </div>
+        </div>
+      <!-------------------------------------------------------------------->
+        <div class="course" id="evaluation">
+            <div class="course_head" id="evaluation_entete">
+                    
+                <div class="question_btn">
+                    <span class="question_label"></span> 
+                    <span class="question_total"></span> :
+                    <span class="question_ordre"></span>
+                    <span class="question_action"></span> 
+                    <span class="question_icon"></span>
+                </div>
+                
+                <div class="repetition_btn">
+                    <span class="repetition_label">ߊ߬ ߟߊߡߍ߲߫ ߕߎ߯ߣߌ߫</span>
+                    <span class="repetition_icon"></span>
+                </div>
+                
+                <div class="correction_btn">
+                    <span class="correction_label">ߏ߬ ߛߊߞߍ߫</span>
+                    <span class="correction_icon"></span>
+                </div>
+            </div> 
+            <div class="course_body" id="evaluation_corps">
+                    
+                <div id='teste_container' align='center'>
+                    <div id='teste_content'>
+                        <div id='reponse_container'><p id='reponse'></p></div>
+                        <div id='check_mark_container'>
+                            <p id='check_mark'></p>
+                            <p id='check_mark_cover'></p>
+                        </div>
+                        <div id='cross_container'><p id=cross></p></div>
+                    </div>
+                    
+                    <div id='teste_annexes_container'>
+                        <div id='alerte'></div>
+                        <div id='autre'></div>
+                    </div>
+                    
                     <div style='display:none'>
-                        <form id='course_form' method='POST' action=''>
-                            <input type='text' name='niveau' id='niveau' value='<?=$niveau?>'/>
-                            <input type='text' name='course_input' id='course_input'/>
-                            <input type='submit' name='submit_course' id='submit_course'/>
+                        <form id='upload_teste_form' method='POST' action='actions.php?get_action=archiver_teste&niveau=<?=$chiffres[$niveau]?>'>
+                            <input type='text' name='teste' id='teste'/>
+                            <input type='text' name='point' id='point'/>
+                            <input type='submit' name='submit' id='submit'/>
                         </form>
                     </div>
                 </div>
-      <!-------------------------------------------------------------------->
-                <div class="course" id="evaluation">
-                    <div class="course_head" id="evaluation_entete">
-                            
-                        <div class="question_btn">
-                            <span class="question_label"></span> 
-                            <span class="question_total"></span> :
-                            <span class="question_ordre"></span>
-                            <span class="question_action"></span> 
-                            <span class="question_icon"></span>
-                        </div>
-                        
-                        <div class="repetition_btn">
-                            <span class="repetition_label">ߊ߬ ߟߊߡߍ߲߫ ߕߎ߯ߣߌ߫</span>
-                            <span class="repetition_icon"></span>
-                        </div>
-                        
-                        <div class="correction_btn">
-                            <span class="correction_label">ߏ߬ ߛߊߞߍ߫</span>
-                            <span class="correction_icon"></span>
-                        </div>
-                    </div> 
-                    <div class="course_body" id="evaluation_corps">
-                            
-                        <div id='teste_container' align='center'>
-                            <div id='teste_content'>
-                                <div id='reponse_container'><p id='reponse'></p></div>
-                                <div id='check_mark_container'>
-                                    <p id='check_mark'></p>
-                                    <p id='check_mark_cover'></p>
-                                </div>
-                                <div id='cross_container'><p id=cross></p></div>
-                            </div>
-                            
-                            <div id='teste_annexes_container'>
-                                <div id='alerte'></div>
-                                <div id='autre'></div>
-                            </div>
-                            
-                            <div style='display:none'>
-                                <form id='upload_teste_form' method='POST' action='actions.php?get_action=archiver_teste&niveau=<?=$chiffres[$niveau]?>'>
-                                    <input type='text' name='teste' id='teste'/>
-                                    <input type='text' name='point' id='point'/>
-                                    <input type='submit' name='submit' id='submit'/>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                    
- 	            <div class='progress_bar'><span class='progress_question_bar'></span><span class='progress_bonne_reponse_bar'></span></div>
-                    
-                    <div class="clavier_container"><?php include "clavier.php"; ?></div>
-                </div>
+            </div>
+            
+         <div class='progress_bar'><span class='progress_question_bar'></span><span class='progress_bonne_reponse_bar'></span></div>
+            
+            <div class="clavier_container"><?php include "clavier.php"; ?></div>
+        </div>
       <!-------------------------------------------------------------------->
         <p class='hand'> &#128070;&#127999; </p>
       <!-------------------------------------------------------------------->
+    </div>
+    <audio id="audio"></audio>
+
+    <script src="http://localhost:8080/kouroukan/js/icones.js"></script>
+    <script src="http://localhost:8080/kouroukan/fonctions.js"></script>
+    <script src="http://localhost:8080/kouroukan/js/caracteres.js"></script>
+    <script src="http://localhost:8080/kouroukan/js/parametres.js"></script>
+    <script src="http://localhost:8080/kouroukan/js/lessons.js"></script>
+    <script src="http://localhost:8080/kouroukan/js/evaluations.js"></script>
+    <script src="http://localhost:8080/kouroukan/js/class.js"></script>
+
+</body>
+</html>
+<?php
+    }else { header("location:programmes.php"); }
+?>
+----------------------------------------------------->
     </div>
     <audio id="audio"></audio>
 

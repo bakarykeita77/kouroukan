9966015bafaae25d0545b53d0463b281116ea8b2
Bakary Keita <bakarykeita77@gmail.com>
2021-11-01 10:26:27 +0000

    Correction du design de Programme

--- js/programmes.js
+++ js/programmes.js
@@ -1,9 +1,10 @@
 
      var programmes_container = $('#programmes_container');
      programmes_container.html(programme());
+     
      function programme(){
       
-         var programme_html = '<h2>ߞߊ߬ߙߊ߲ ߢߍߥߟߊ </h2>';
+         var programme_html = '<h2>ߘߋ߰ߟߌ ߢߍߥߟߊ </h2>';
          programme_html += '<ul>';
          for(var i=0;i<liste_de_matieres.length;i++){
              programme_html += '<li><a href="lesson.php?matiere_id='+liste_de_matieres[i][0]+'&matiere_nom='+liste_de_matieres[i][1]+'&niveau='+i+'">'+liste_de_matieres[i][1]+'</a></li>';

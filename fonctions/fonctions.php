0a11d61bf091f2a524669ce1d835984268c9bf76
Bakary <bakarykeita77@gmail.com>
2022-02-24 13:54:53 +0000

    Groupement de toutes les fonctions dans un dossier

--- fonctions/fonctions.php
+++ fonctions/fonctions.php
@@ -1,22 +1,134 @@
-<script>
-
-	function afficher(element) {
-	    element.css({ 'display':'block', 'height':0 });
-		element.css({ 'display':'block' });
-		element.animate({ 'height':'25vh' }, 200);
-	}
-	function afficher_en_jailli( element,largeur,hauteur ) {
-	//	element.css({ 'width':0, 'height':0 });
-		display(element);
-		element.animate({ 'width':'90%', 'height':'25vh' }, 600);
-	}
-	function aggrandir_caractere_de(element) { element.css( 'font-size','+=32px' ); }
-	function appetir_caractere_de(element) { element.css( 'font-size','-=32px' ); }
-	function centrer(element) { element.addClass('center'); }
-	function display(element) { element.css('display','block'); }
-	function fermer(element) {
-	    element.animate({ 'height':0 }, 200);
-	    setTimeout((function(){ element.css({ 'display':'none' }) }),180);
-	}
+<?php 
 	
-</script>
+	$server = "localhost";
+	$login = "root";
+	$pass = "";
+
+	try{
+		$db = new PDO("mysql:host=$server;dbname=kouroukan;charset=utf8",$login,$pass);
+		$db -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+		function ajouterClient($prenom, $nom, $naissance, $sexe, $adresse, $email, $password){
+			global $db;
+
+			$sql = "INSERT INTO users(prenom, nom, naissance, sexe, adresse, email, password)
+					VALUES(:prenom, :nom, :naissance, :sexe, :adresse, :email, :password)
+			";
+
+			$requette = $db->prepare($sql);
+
+			$requette->bindValue(':prenom',   $prenom,   PDO::PARAM_STR);
+			$requette->bindValue(':nom',      $nom,      PDO::PARAM_STR);
+			$requette->bindValue(':naissance',$naissance,PDO::PARAM_STR);
+			$requette->bindValue(':sexe',     $sexe,     PDO::PARAM_STR);
+			$requette->bindValue(':adresse',  $adresse,  PDO::PARAM_STR);
+			$requette->bindValue(':email',    $email,    PDO::PARAM_STR);
+			$requette->bindValue(':password', $password, PDO::PARAM_STR);
+
+			$utilisateurs =  $requette->execute();
+			return $utilisateurs;
+		 }
+		function archiverLesson($client_id,$niveau,$course){
+		function archiverNotes($numero, $question, $reponse, $points){
+			global $db;
+
+			$sql = "INSERT INTO notes(numero, question, reponse, points) 
+					VALUES(:numero, :question, :reponse, :points)";
+			$requette = $db -> prepare($sql);
+
+			$requette->bindValue(':numero',  $numero,   PDO::PARAM_STR);
+			$requette->bindValue(':question',$question, PDO::PARAM_STR);
+			$requette->bindValue(':reponse', $reponse,  PDO::PARAM_STR);
+			$requette->bindValue(':points',  $points,   PDO::PARAM_STR);
+
+			$notes = $requette->execute();
+
+			return $notes;
+		}
+		function getAllClients(){
+			global $db;
+
+			$sql = "SELECT * FROM users ORDER BY id DESC LIMIT 12";
+
+			$requette = $db->prepare($sql);
+			$requette->execute();
+			return $utilisateurs = $requette->fetchAll(PDO::FETCH_ASSOC);
+		 }
+		function getClient($id){
+			global $db;
+
+			$sql = "SELECT * FROM users WHERE id=:id";
+
+			$requette = $db->prepare($sql);
+			$requette->bindValue(':id',$id,PDO::PARAM_INT);
+			$requette->execute();
+			return $utilisateurs = $requette->fetch(PDO::FETCH_ASSOC);
+		 }
+		function getEmailsAndPasswords(){
+			global $db;
+
+			$sql = "SELECT email, password FROM users";
+			$requette = $db->prepare($sql);
+			$requette->execute();
+
+			$emailsAndPass = $requette->fetchAll(PDO::FETCH_ASSOC);
+
+			return $emailsAndPass;
+		 }
+		function modifierClient($id, $prenom, $nom, $naissance, $sexe, $adresse, $email, $password){
+			global $db;
+
+			$sql = "UPDATE users SET prenom=:prenom, nom=:nom, naissance=:naissance, sexe=:sexe, adresse=:adresse, email=:email, password=:password WHERE id=:id";
+
+			$requette = $db->prepare($sql);
+
+			$requette->bindValue(':id',       $id,       PDO::PARAM_INT);
+			$requette->bindValue(':prenom',   $prenom,   PDO::PARAM_STR);
+			$requette->bindValue(':nom',      $nom,      PDO::PARAM_STR);
+			$requette->bindValue(':naissance',$naissance,PDO::PARAM_STR);
+			$requette->bindValue(':sexe',     $sexe,     PDO::PARAM_STR);
+			$requette->bindValue(':adresse',  $adresse,  PDO::PARAM_STR);
+			$requette->bindValue(':email',    $email,    PDO::PARAM_STR);
+			$requette->bindValue(':password', $password, PDO::PARAM_STR);
+
+			$utilisateurs =  $requette->execute();
+			return $utilisateurs;
+		 }
+		function nbrEmails(){
+			global $db;
+
+			$sql = "SELECT COUNT(email) FROM users";
+			$requette = $db->prepare($sql);
+			$requette->execute();
+			$mails = $requette->fetchAll(PDO::FETCH_ASSOC);
+			$nbr_emails = $mails[0]['COUNT(email)'];
+
+			return $nbr_emails;
+		 }
+		function passCorrespondant($client_email){
+			global $db;
+
+			$sql = "SELECT password FROM users WHERE email=:email";
+			$requette = $db->prepare($sql);
+			$requette->bindValue(':email',$client_email,PDO::PARAM_STR);
+			$requette->execute();
+			$pass_table= $requette->fetchAll(PDO::FETCH_ASSOC);
+			$pass_correspondant = $pass_table[0]['password'];
+
+			return $pass_correspondant;
+		 }
+		function supprimerClient($id){
+			global $db;
+
+			$sql = "DELETE FROM users WHERE id=:id";
+
+			$requette = $db->prepare($sql);
+			$requette->bindValue(':id',   $id,   PDO::PARAM_INT);
+			$utilisateurs =  $requette->execute();
+			return $utilisateurs;
+		 }
+	 }
+	catch(PDOException $e){
+		echo "Echec :".$e -> getMessage();
+	 }
+ ?>

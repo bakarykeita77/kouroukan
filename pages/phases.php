3e0e9bf7cb481dd060ba0302c94d213822c21e0f
Bakary <bakarykeita77@gmail.com>
2022-03-13 20:35:40 +0000

    Quelques corrections

---
+++ pages/phases.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html >
+<html>
+    <head>
+        <title>phases</title>
+        <meta charset = "utf-8"/>
+        <link rel="stylesheet" href="phases.css"/>
+    </head>
+    <body>
+        
+    </body>
+
+</html>

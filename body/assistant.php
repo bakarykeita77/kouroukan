36b19cd4dfb8c86c993e7258844f42d93d683d0a
Fawali Keita <keitafawali72@gmail.com>
2022-07-08 13:41:04 +0000

    Création de body.php

---
+++ body/assistant.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    	<title>assistant</title>
+    	<meta charset = "utf-8">
+    	<style>
+    	<?php include "css/assistant.css"; ?>
+    	</style>
+    </head>
+    
+    <body>
+	
+		<div class = "center" id="assistant">
+			<span id="fermer_assistant">&times;</span>
+			<p id="titre_assistant"></p>
+			<div id="corps_assistant">
+				<p id="avertissement">ߦߴߊ߬ ߡߊ߬ ߞߵߊ߬ ߛߓߍ߫ ߞߏ߫ <span id="erreur"> <span id="ecrit_correcte"></span><span class="clignote" id="ecrit_errone"></span> </span></p>
+				<p id="message_erreur"></p>
+				
+				<p id="correction_erreur"></p>
+				<p id="reference"></p>
+			</div>		
+		</div>
+		
+	
+    </body>
+</html>

972769f04bd8261b5c4f18e9a80c7afe3acb9e53
Fawali Keita <keitafawali@gmail.com>
2023-02-05 20:17:14 +0100

    Responnsivité  des pages de connexion et d'inscription

--- pages/connexion.php
+++ pages/connexion.php
@@ -12,32 +12,36 @@
 <body>
 	<div><?php include("tete-de-page.php"); ?></div>    
     <div class="cover">
-      <!----------------------------------------------------------------------------------------------------->  
-        <div id = "connexion_form">
-    		<h2>ߜߊ߲߬ߞߎ߲߬ ߥߟߊ</h2>
-    		
-    		<form action="accueil.php" method="POST" id="formulaire_de_connexion">
-    			<div class="input_box">
-    				<input type="email" autocomplete="off" name="client_email" class="connexion_input" id="client_email" required />
-    				<label>Email</label>
-    			</div>
-    			<div class="input_box">
-    				<input type="password" autocomplete="off" name="client_pass" class="connexion_input" id="client_pass" required />
-    				<label>ߜߎ߲߬ߘߎ߬ߕߐ߮</label>
-    			</div>
-    			<div id="button_box">
-    				<input type="submit" name="submit" id="connexion_btn" value=" ߜߊ߲߬ߞߎ߲߬ߠߌ ߞߍ߫"/>
-    			</div>
-    		</form>
-    	</div>
-    	
-		<div class="note_container">
-			<h1 class="note_titre">ߞߙߎ߬ߞߊ߲߬ ߞߣߍ ߜߊ߲߬ߞߎ߲߬ߠߌ</h1>
-			<div class="note">
-				<p> ߌ ߜߊ߲߬ߞߎ߲߬ ߞߙߎ߬ߞߊ߲߬ ߠߊ߫߸  ߕߊ߲߬ߘߐ߸ ߞߊ߬ ߛߊ߲߬ߓߊ߬ߕߐ߮ ߣߌ߫ ߜߎ߲߬ߘߎ߬ߕߐ߮ ߟߎ߬ ߟߝߊ߫߸ ߞߵߊ߬ ߟߊߓߊ߲߫ ߘߎ߰ߡߊ߬ ߞߘߎ ߘߌ߲߯ ߡߊ߬. ߌ ߜߊ߲߬ߞߎ߬ߣߍ߲ ߠߴߏ߬ ߘߌ߫.</p>
-				<p> ߣߌ߫ ߜߊ߲߬ߞߎ߲߬ߠߌ ߡߊ߫ ߢߊ߬߸ ߒ߬ߓߵߊ߬ ߘߌ߫ ߛߐ߬ߘߐ߲߬ ߝߌ߬ߟߌ ߦߋ߫ ߛߊ߲߬ߓߊ߬ߕߐ߮ ߘߐ߫ ߥߟߊ ߜߎ߲߬ߘߎ߬ߕߐ߮ ߘߐ߫߸ ߏ߬ߘߐ߬߸ ߦߋ߫ ߛߴߊ߬ ߡߊ߬ </p>
-				<p>ߣߌ߫ ߝߟߌ߬ ߡߊ߫ ߞߍ߫ ߛߊ߲߬ߓߊ߬ߕߐ߮ ߣߌ߫ ߜߎ߲߬ߘߎ߬ߕߐ߮ ߛߌ߫ ߘߐ߫߸ ߒ߬ߓߴߊ߬ ߘߌ߫ ߛߐ߬ߘߐ߲߬ ߌ ߕߐ߯ ߕߍ߫ ߘߋ߰ߘߋ߲ ߠߎ߬ ߛߙߍߘߍ ߘߐ߫߸ ߏ߬ߘߐ߬ ߦߋ߫ <a href="inscription.php"> ߕߐ߯ߛߓߍߟߌ ߞߍ߫ </a></p>
+		 
+        <div id="connexion_container">
+
+			<div id="connexion_note">
+				<h1 class="note_titre">ߞߙߎ߬ߞߊ߲߬ ߞߣߍ ߜߊ߲߬ߞߎ߲߬ߠߌ</h1>
+				<div class="note">
+					<p> ߌ ߜߊ߲߬ߞߎ߲߬ ߞߙߎ߬ߞߊ߲߬ ߠߊ߫߸  ߕߊ߲߬ߘߐ߸ ߞߊ߬ ߛߊ߲߬ߓߊ߬ߕߐ߮ ߣߌ߫ ߜߎ߲߬ߘߎ߬ߕߐ߮ ߟߎ߬ ߟߝߊ߫߸ ߞߵߊ߬ ߟߊߓߊ߲߫ ߘߎ߰ߡߊ߬ ߞߘߎ ߘߌ߲߯ ߡߊ߬. ߌ ߜߊ߲߬ߞߎ߬ߣߍ߲ ߠߴߏ߬ ߘߌ߫.</p>
+					<p> ߣߌ߫ ߜߊ߲߬ߞߎ߲߬ߠߌ ߡߊ߫ ߢߊ߬߸ ߒ߬ߓߵߊ߬ ߘߌ߫ ߛߐ߬ߘߐ߲߬ ߝߌ߬ߟߌ ߦߋ߫ ߛߊ߲߬ߓߊ߬ߕߐ߮ ߘߐ߫ ߥߟߊ ߜߎ߲߬ߘߎ߬ߕߐ߮ ߘߐ߫߸ ߏ߬ߘߐ߬߸ ߦߋ߫ ߛߴߊ߬ ߡߊ߬ </p>
+					<p>ߣߌ߫ ߝߟߌ߬ ߡߊ߫ ߞߍ߫ ߛߊ߲߬ߓߊ߬ߕߐ߮ ߣߌ߫ ߜߎ߲߬ߘߎ߬ߕߐ߮ ߛߌ߫ ߘߐ߫߸ ߒ߬ߓߴߊ߬ ߘߌ߫ ߛߐ߬ߘߐ߲߬ ߌ ߕߐ߯ ߕߍ߫ ߘߋ߰ߘߋ߲ ߠߎ߬ ߛߙߍߘߍ ߘߐ߫߸ ߏ߬ߘߐ߬ ߦߋ߫ <a href="inscription.php"> ߕߐ߯ߛߓߍߟߌ ߞߍ߫ </a></p>
+				</div>
 			</div>
+
+			<div id = "connexion_form">
+				<h2>ߜߊ߲߬ߞߎ߲߬ ߥߟߊ</h2>
+				
+				<form action="accueil.php" method="POST" id="formulaire_de_connexion">
+					<div class="input_box">
+						<input type="email" autocomplete="off" name="client_email" class="connexion_input" id="client_email" required />
+						<label>Email</label>
+					</div>
+					<div class="input_box">
+						<input type="password" autocomplete="off" name="client_pass" class="connexion_input" id="client_pass" required />
+						<label>ߜߎ߲߬ߘߎ߬ߕߐ߮</label>
+					</div>
+					<div id="button_box">
+						<input type="submit" name="submit" id="connexion_btn" value=" ߜߊ߲߬ߞߎ߲߬ߠߌ ߞߍ߫"/> 
+					</div>
+				</form>
+			</div>
+			
 		</div>
 	</div>
 	
--- pages/inscription.php
+++ pages/inscription.php
@@ -10,55 +10,59 @@
 <body>
 	<div><?php include("tete-de-page.php"); ?></div>    
     <div class="cover">
-    	<div id="inscription_form">
-    		
-    		<h2>ߕߐ߯ߛߓߍ߫ ߥߟߊ</h2>
-    		
-    		<form action="/kouroukan/pages/actions.php" method="POST" id="formulaire_de_connexion">
-    			
-    			<div class="input_box">
-    				<input type="text" autocomplete="off" name="prenom" class="inscription_input" id="prenom" required />
-    				<label>ߕߐ߮</label>
-    			</div>
-    			<div class="input_box">
-    				<input type="text" autocomplete="off" name="nom" class="inscription_input" id="nom" required />
-    				<label>ߖߊ߬ߡߎ߲</label>
-    			</div>
-    			<div class="input_box">
-    				<input type="text" autocomplete="off" name="naissance" class="inscription_input" id="naissance" required />
-    				<label>ߡߐߦߌߕߎߡߊ</label>
-    			</div>
-    			<div class="input_box">
-    				<input type="text" autocomplete="off" name="sexe" class="inscription_input" id="sexe" required />
-    				<label>ߖߊ߲߭</label>
-    			</div>
-    			<div class="input_box">
-    				<input type="text" autocomplete="off" name="adresse" class="inscription_input" id="adresse" required />
-    				<label>ߜߎ߲߬ߘߎ߬ߕߐ߮</label>
-    			</div>
-    			<div class="input_box">
-    				<input type="email" autocomplete="off" name="email" class="inscription_input" id="email" required />
-    				<label>E-mail</label>
-    			</div>
-    			<div class="input_box">
-    				<input type="password" autocomplete="off" name="pass" class="inscription_input" id="client_pass" required />
-    				<label>ߜߎ߲߬ߘߎ߬ߕߐ߮</label>
-    			</div>
-    			<div id="button_box">
-    				<input type="submit" name="submit" id="inscription_btn" value="ߕߐ߯ߛߓߍߟߌ ߞߍ߫"/>
-    			</div>
-    		</form>
+		<div id="inscription_container">
 
-    		 <p class="error_message" align="center"><?php if(isset($error)) { echo $error; } ?></p>
-    	</div>
-    	<div class="note_container">
-    	    <h1 class="note_titre">ߞߙߎ߬ߞߊ߲߬ ߞߣߍ ߕߐ߯ߛߓߍߟߌ</h1>
-			<div class="note">
-				<p>ߞߙߎ߬ߞߊ߲߬ ߞߣߍ ߞߊ߬ߙߊ߲ ߦߋ߫ ߘߊߡߌ߬ߘߊ߬ ߟߊ߫ ߕߐ߯ߛߓߍߟߌ ߟߋ߬ ߝߟߐ߫ ߡߊ߬</p>
-				<p>ߏ߬ߘߐ߬߸ ߕߐ߯ߛߓߍߟߌ߫ ߥߟߊ ߢߌ߲߬ ߠߝߊ߫߸ ߦߋ߫ ߘߎ߰ߡߊ߬ ߞߘߎ ߘߌ߲߯߸ ߕߐ߯ߛߓߍߟߌ ߓߊ߲ߣߍ߲ ߠߴߏ߬ ߘߌ߫. ߏ߬ߟߋ ߞߐ߫߸ ߌ ߘߌ߫ ߛߋ߫ ߞߵߌ ߜߊ߲߬ߞߎ߲߬. ߞߙߎ߬ߞߊ߲߬ ߞߣߍ ߓߌ߬ߟߌ߬ߣߍ߲ ߠߴߌ ߛߊ߬ߥߏ ߘߴߏ߬ ߘߌ߫ ߕߎ߬ߡߊ߫-ߊ߫-ߕߎ߬ߡߊ߫ ߞߵߌ ߘߌߦߊ߫߸ ߦߙߐ߫-ߊ߫-ߦߙߐ߫ ߞߵߌ ߘߌߦߊ߫.</p>
+			<div id="inscription_form">
+				
+				<h2>ߕߐ߯ߛߓߍ߫ ߥߟߊ</h2>
+				
+				<form action="/kouroukan/pages/actions.php" method="POST" id="formulaire_de_connexion">
+					
+					<div class="input_box">
+						<input type="text" autocomplete="off" name="prenom" class="inscription_input" id="prenom" required />
+						<label>ߕߐ߮</label>
+					</div>
+					<div class="input_box">
+						<input type="text" autocomplete="off" name="nom" class="inscription_input" id="nom" required />
+						<label>ߖߊ߬ߡߎ߲</label>
+					</div>
+					<div class="input_box">
+						<input type="text" autocomplete="off" name="naissance" class="inscription_input" id="naissance" required />
+						<label>ߡߐߦߌߕߎߡߊ</label>
+					</div>
+					<div class="input_box">
+						<input type="text" autocomplete="off" name="sexe" class="inscription_input" id="sexe" required />
+						<label>ߖߊ߲߭</label>
+					</div>
+					<div class="input_box">
+						<input type="text" autocomplete="off" name="adresse" class="inscription_input" id="adresse" required />
+						<label>ߜߎ߲߬ߘߎ߬ߕߐ߮</label>
+					</div>
+					<div class="input_box">
+						<input type="email" autocomplete="off" name="email" class="inscription_input" id="email" required />
+						<label>E-mail</label>
+					</div>
+					<div class="input_box">
+						<input type="password" autocomplete="off" name="pass" class="inscription_input" id="client_pass" required />
+						<label>ߜߎ߲߬ߘߎ߬ߕߐ߮</label>
+					</div>
+					<div id="button_box">
+						<input type="submit" name="submit" id="inscription_btn" value="ߕߐ߯ߛߓߍߟߌ ߞߍ߫"/>
+					</div>
+				</form>
+
+				<p class="error_message" align="center"><?php if(isset($error)) { echo $error; } ?></p>
 			</div>
+
+			<div id="inscription_note">
+				<h1 class="note_titre">ߞߙߎ߬ߞߊ߲߬ ߞߣߍ ߕߐ߯ߛߓߍߟߌ</h1>
+				<div class="note">
+					<p>ߞߙߎ߬ߞߊ߲߬ ߞߣߍ ߞߊ߬ߙߊ߲ ߦߋ߫ ߘߊߡߌ߬ߘߊ߬ ߟߊ߫ ߕߐ߯ߛߓߍߟߌ ߟߋ߬ ߝߟߐ߫ ߡߊ߬</p>
+					<p>ߏ߬ߘߐ߬߸ ߕߐ߯ߛߓߍߟߌ߫ ߥߟߊ ߢߌ߲߬ ߠߝߊ߫߸ ߦߋ߫ ߘߎ߰ߡߊ߬ ߞߘߎ ߘߌ߲߯߸ ߕߐ߯ߛߓߍߟߌ ߓߊ߲ߣߍ߲ ߠߴߏ߬ ߘߌ߫. ߏ߬ߟߋ ߞߐ߫߸ ߌ ߘߌ߫ ߛߋ߫ ߞߵߌ ߜߊ߲߬ߞߎ߲߬. ߞߙߎ߬ߞߊ߲߬ ߞߣߍ ߓߌ߬ߟߌ߬ߣߍ߲ ߠߴߌ ߛߊ߬ߥߏ ߘߴߏ߬ ߘߌ߫ ߕߎ߬ߡߊ߫-ߊ߫-ߕߎ߬ߡߊ߫ ߞߵߌ ߘߌߦߊ߫߸ ߦߙߐ߫-ߊ߫-ߦߙߐ߫ ߞߵߌ ߘߌߦߊ߫.</p>
+				</div>
+			</div>
+
     	</div>
-    	
 	    <script src="/kouroukan/js/jquery-3.3.1.js"></script>
     	<script src="/kouroukan/note-fonctions.js"></script>
     	<script src="/kouroukan/js/inscription.js"></script>

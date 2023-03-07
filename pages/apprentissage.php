81a2a0a1d3fb85170e5c9c56c7da7b8cbffedf05
Bakary Keita <bakarykeita77@gmail.com>
2023-03-07 20:40:02 +0000

    quelques correctons

---
+++ pages/apprentissage.php
@@ -0,0 +1,37 @@
+
+	<link rel="stylesheet" href="/kouroukan/css/apprentissage.css"/>
+
+    <div class="course_head" id="apprentissage_head">
+        <div class = 'progress_bar' id = "apprentissage_progress_bar">
+            <div class='progress_bonne_reponse_bar'></div>
+        </div>
+    </div>
+    <div class="course_body" id="apprentissage_body"></div>
+    <div class="course_foot" id="apprentissage_foot">
+        <div class="dialogue_btn" id="apprentissage_dialogue_btn">
+
+            <div class="btns media" id="media_apprentissage">
+                <div class="media_btns">
+                    <div class='btn'>
+                        <span class='play_icon'>&#9664;</span>
+                        <span class='play_label'>ߝߐߟߊ߲</span>
+                    </div>
+
+                    <div class='btn'>
+                        <span class='stop_icon'>&#9632;</span>
+                        <span class='stop_label'>ߘߊ߬ߘߋ߬ߟߊ߲ </span> 
+                    </div>
+                </div>
+                <div class="btns_label media_label">ߝߊߟߊ߲ߞߏ</div>
+            </div>
+
+            <div class="btns parametre" id="parametre_lesson">
+                <div class='parametres_btns' id='parametre_lesson_btn'>
+                    <span class='parametre_icon'>&#9881;</span>
+                    <span class='parametre_label'>ߛߏ߯ߙߏߟߊ߲</span>  
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script src="/kouroukan/js/apprentissage.js"></script>
---
+++ pages/exercice.php
@@ -0,0 +1,13 @@
+
+	<link rel="stylesheet" href="/kouroukan/css/exercice.css"/>
+
+	<div class="course_head" id="exercice_head">
+		<div class='progress_bar' id="exercice_progress_bar">
+			<p class='progress_question_bar'></p>
+			<p class='progress_bonne_reponse_bar'></p>
+		</div>
+	</div>
+	<div class="course_body" id="exercice_body"></div>   <!--Cette division est chargé par la fonction chargerExercice() dans lesson.js-->
+	<div class="course_foot" id="exercice_foot"></div>
+	
+    <script src="/kouroukan/js/exercice.js"></script>

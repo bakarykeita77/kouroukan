35c20e5844c34f10da3636546c1c9e34a418aa54
lenovo_all_in_one <bakarykeita77@gail.com>
2026-01-23 18:38:46 +0100

    Modification de design

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="nqo">
 
 <head>
  	<title>ߞߙߎ߬ߞߊ߲߬</title>
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+Nko&display=swap">
   	<link rel="stylesheet" href="css/index.css"/>
 </head>
 
